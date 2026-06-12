cf985f6d533d2e85670ae659b2b048916ab19234
Seba <seba@Sebas-MacBook-Air.local>
2026-06-12 16:30:48 +0200

    solved 3rd square root bound, skipping evens approach

--- euler_problems/0001-0020/euler0003.py
+++ euler_problems/0001-0020/euler0003.py
@@ -4,12 +4,11 @@ from timer_utils import timer
 
 @timer  # O(√n)
 def solution(n=600_851_475_143):
-    factor = 3
+    factor = 2
     while factor * factor <= n:
         while n % factor == 0:
             n //= factor
         factor = 3 if factor == 2 else factor + 2
-
     return n
 
 
